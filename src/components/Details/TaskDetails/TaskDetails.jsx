10042dc1b7ed860fa3b178071711ff2b3e3a153c
brenopinna <brenopinna1008@gmail.com>
2022-09-04 20:13:52 -0300

    styled-components no TaskDetails

--- src/components/Details/TaskDetails/TaskDetails.jsx
+++ src/components/Details/TaskDetails/TaskDetails.jsx
@@ -1,47 +1,20 @@
 import { React } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
 
-import { Button } from '../../../Global/styles'
+import { BackButton, DetailsContainer, DetailsTitle, DetailsText } from './styles'
 
-const TaskDetails = ({ className }) => {
+const TaskDetails = () => {
    const { taskTitle } = useParams();
    const navigate = useNavigate();
    return (
-      <div className={className}>
-         <div className="back-button-container">
-            <Button onClick={() => navigate('../')}>Voltar</Button>
-         </div>
-         <div className="details-container">
-            <h2 className="title">{taskTitle}</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa atque voluptate libero accusantium exercitationem! Rerum.</p>
-         </div>
-      </div>
+      <>
+         <BackButton onClick={() => navigate('../')}>Voltar</BackButton>
+         <DetailsContainer>
+            <DetailsTitle>{taskTitle}</DetailsTitle>
+            <DetailsText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa atque voluptate libero accusantium exercitationem! Rerum.</DetailsText>
+         </DetailsContainer>
+      </>
    );
 }
 
-const StyledTaskDetails = styled(TaskDetails)`
-   .back-button-container{
-      margin-top: 10px;
-      width: 130px;
-      height: 40px;
-   }
-
-   .details-container{
-      background-color: #444;
-      border-radius: 5px;
-      margin-top: 15px;
-      padding: 15px 20px;
-   }
-
-   .details-container .title{
-      color: chartreuse;
-   }
-
-   .details-container p{
-      margin-top: 5px;
-      color: #eee;
-   }
-`;
-
-export default StyledTaskDetails;
+export default TaskDetails;
---
+++ src/components/Details/TaskDetails/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { Button } from '../../../Global/styles';
+
+export const BackButton = styled(Button)`
+   margin-top: 10px;
+   width: 130px;
+   height: 40px;
+`;
+
+export const DetailsContainer = styled.div`
+   background-color: #444;
+   border-radius: 5px;
+   margin-top: 15px;
+   padding: 15px 20px;
+`;
+
+export const DetailsTitle = styled.h2`
+   color: chartreuse;
+`;
+
+export const DetailsText = styled.p`
+   margin-top: 5px;
+   color: #eee;
+`;
